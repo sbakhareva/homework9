d65996ca9ac727de6b43dcc91b14e90139bac5b4
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-10-04 11:59:56 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {1, 2, 3};
+        int sum = 0;
+        for (int index = 0; index < arr.length; index++) {
+            sum += arr[index];
+            System.out.println(sum);
+        }
     }
 }
